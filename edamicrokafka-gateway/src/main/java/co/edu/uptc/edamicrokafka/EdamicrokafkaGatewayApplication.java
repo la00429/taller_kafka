31ae799a5cead234e3c55538d98f2354f6ffe4e2
Laura Figueredo <laura.figueredo02@uptc.edu.co>
2025-10-06 22:32:28 -0500

    fix: Replace Netflix Zuul with Spring Cloud Gateway for better compatibility

--- edamicrokafka-gateway/src/main/java/co/edu/uptc/edamicrokafka/EdamicrokafkaGatewayApplication.java
+++ edamicrokafka-gateway/src/main/java/co/edu/uptc/edamicrokafka/EdamicrokafkaGatewayApplication.java
@@ -1,11 +1,9 @@
-package co.edu.uptc.edamicrokafka.gateway;
+package co.edu.uptc.edamicrokafka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableZuulProxy
 public class EdamicrokafkaGatewayApplication {
 
 	public static void main(String[] args) {
